f5944be3cc15e0c5797c5b8b60ce4ad3e934840d
pjonski <pawel.jonski@enigma.com.pl>
2015-12-14 12:35:32 +0100

    char drive v0.2.9 read

--- hello-jonek.c
+++ hello-jonek.c
@@ -35,7 +35,7 @@ static char output[] = "Volenti non fit iniuria.\n";
 ssize_t my_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
 {
 
-
+/*
 if (output[*f_pos] == '\0') {
         printk(KERN_INFO "End of string, returning zero.\n");
         return 0;
@@ -46,18 +46,18 @@ if (output[*f_pos] == '\0') {
     return 1;  // returned a single character
 
 
-/*
+*/
 	int str_len=strlen(output);
 	int not_copied_bytes;
 	printk("%d %d\n",str_len, *f_pos);
-	not_copied_bytes= copy_to_user(buf, &output[*f_pos], 5);
+	not_copied_bytes= copy_to_user(buf, output[*f_pos], str_len);
 
 	printk("%d\n",not_copied_bytes);
 	if(!not_copied_bytes)
 		return 0;
 	*f_pos+=not_copied_bytes;
 	return 1;
-*/
+
 }
 
 
